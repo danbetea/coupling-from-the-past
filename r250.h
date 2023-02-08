f0d361cefcf685397664ff743617b09eeb8b1468
Dan Betea <87662450+danbetea@users.noreply.github.com>
2023-02-08 15:12:12 +0100

    Delete r250.h

--- r250.h
+++
@@ -1,7 +0,0 @@
-/*** Function prototypes for R250 random number generator, by W. L. Maier  ***/
-unsigned int r250();
-unsigned int r250n(unsigned n);
-double dr250();
-void r250_init(int seed);
-
-
